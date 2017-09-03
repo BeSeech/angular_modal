44946e2fd67db90212e966824afda18a3bef4843
beseech <beseechmv@evos.com.ua>
2017-09-03 19:08:29 +0300

    With button's callbacks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {Popup} from './ng2-opd-popup';
+import {LogsService} from './logs/logs.service';
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-root',
@@ -9,9 +11,26 @@ import {Popup} from './ng2-opd-popup';
 export class AppComponent {
   title = 'app';
   @ViewChild('p1') p1: Popup;
+  constructor(private log: LogsService) {
+
+  }
+
+  confirm() {
+    this.log.success('Confirm');
+  }
+
+  cancel() {
+    this.log.error('Cancel');
+  }
 
   bClick() {
-    this.p1.show();
+//    const keyUpObservable = Observable.fromEvent(this.p1, 'confirmClick');
+    const options = {
+        header: 'Pay attention!',
+        color: 'red'
+    };
+    this.p1.show(options);
+    this.log.info('Show dialog');
   }
 }
 
